5ca8f1f2b5d8dc1b1cc84efb677db9b041fe42a3
Fadliaminhr <fadliaminhr@gmail.com>
2023-08-01 13:49:38 +0700

    Testscript Unggah Data

--- cypress/e2e/Uploadfile.cy.js
+++ cypress/e2e/Uploadfile.cy.js
@@ -36,7 +36,25 @@ it('File Upload Test', function(){
 
     // Select 'Produk' from dropdown
     cy.get(':nth-child(6) > .ant-col > .ant-form-item-control > .ant-form-item-children > .ant-select > .ant-select-selection').click()
-    cy.wait(2000)
     cy.contains('.ant-select-dropdown-menu-item.ant-select-dropdown-menu-item-active','Kartu Bni').click()
+
+    // Select 'Template Unggah' from dropdown 
+    cy.get(':nth-child(2) > [style="margin-right: 20px; margin-left: 20px;"] > :nth-child(2) > .ant-col > .ant-form-item-control > .ant-form-item-children > .ant-select > .ant-select-selection').click()
+    cy.contains('Format CSV BNI (.csv)').click()
+
+    // Unggah File
+    cy.get('.ant-input-affix-wrapper > .ant-input').attachFile('_B N I Format CSV BNI DEV 1.csv')
     
+    // Select 'Tanggal Kirim'
+    cy.get('[style="margin-top: 30px;"] > .ant-row > .ant-col > .ant-form-item-control > .ant-form-item-children > .ant-calendar-picker > div > .ant-calendar-picker-input').click()
+    cy.get('.ant-calendar-today-btn').click()
+
+    // Select 'Tanggal Upload'
+    cy.get(':nth-child(6) > .ant-row > .ant-col > .ant-form-item-control > .ant-form-item-children > .ant-calendar-picker > div > .ant-calendar-picker-input').click()
+    cy.get('.ant-calendar-today-btn').click()
+
+    // Click button 'Lanjut'
+    cy.get('[style="float: right;"] > .ant-btn').click()
+    cy.wait(8000)
+    cy.get('[style="margin-top: 20px; float: right; padding-right: 20px;"] > .ant-btn').click()
   });
